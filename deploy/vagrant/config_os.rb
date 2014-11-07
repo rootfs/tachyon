51870406660cacb8aa88a08b44d2d1817aa0b359
rootfs <hchen@redhat.com>
2014-11-07 13:15:18 -0500

    initial openstack integration
    Signed-off-by: rootfs <hchen@redhat.com>

---
+++ deploy/vagrant/config_os.rb
@@ -0,0 +1,23 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# OpenStack specific configurations go here
+
+def config_os(config, i, total, name)        
+  config.vm.provider :openstack do |os|
+    os.username     = ENV['OS_USERNAME']
+    os.api_key      = ENV['OS_PASSWORD']
+    os.flavor       = FLAVOR
+    os.image        = /CentOS/
+    os.endpoint     = KEYSTONE
+    os.security_groups = SECURITY_GROUP
+    os.ssh_username = SSH_USERNAME
+    os.keypair_name = KEYPAIR_NAME
+    os.floating_ip = "auto"
+    os.server_name = name
+    if i == total # last VM starts tachyon
+      config.vm.provision "shell", path: Post
+      config.vm.provision "shell", path: "start_tachyon_cluster.sh"
+    end
+  end
+end
